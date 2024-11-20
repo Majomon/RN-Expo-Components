e32e337d7edc185be648017b14c9f8e109441153
Mauri <mauri.monzon91@gmail.com>
2024-11-20 16:36:30 -0300

    Animacion fadein y fadeout

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,15 +1,41 @@
 import { ThemeButton } from "@/presentation/shared/ThemeButton";
 import { ThemeView } from "@/presentation/shared/ThemeView";
-import { View, Text } from "react-native";
+import { useRef } from "react";
+import { Animated, View } from "react-native";
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <ThemeView margin>
-      <Text>Animation101Screen</Text>
-      <ThemeButton className="mb-5" onPress={() => console.log("Fade In")}>
+    <ThemeView margin className="flex-1 justify-center items-center">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+        }}
+      />
+      <ThemeButton className="my-5" onPress={fadeIn}>
         FadeIn
       </ThemeButton>
-      <ThemeButton className="mb-5" onPress={() => console.log("Fade Out")}>
+      <ThemeButton className="my-5" onPress={fadeOut}>
         FadeOut
       </ThemeButton>
     </ThemeView>
