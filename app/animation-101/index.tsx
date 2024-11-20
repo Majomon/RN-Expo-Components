17261649f72fca5974a552a93b0c144ed7083b9d
Mauri <mauri.monzon91@gmail.com>
2024-11-20 16:26:59 -0300

    themeButton

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native';
+import { ThemeButton } from "@/presentation/shared/ThemeButton";
+import { ThemeView } from "@/presentation/shared/ThemeView";
+import { View, Text } from "react-native";
 
 const Animation101Screen = () => {
   return (
-    <View>
+    <ThemeView margin>
       <Text>Animation101Screen</Text>
-    </View>
+      <ThemeButton className="mb-5" onPress={() => console.log("Fade In")}>
+        FadeIn
+      </ThemeButton>
+      <ThemeButton className="mb-5" onPress={() => console.log("Fade Out")}>
+        FadeOut
+      </ThemeButton>
+    </ThemeView>
   );
 };
 export default Animation101Screen;
