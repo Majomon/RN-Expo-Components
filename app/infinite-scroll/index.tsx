df213ed966a93b85cf709435c4565cece34ca7c9
Mauri <mauri.monzon91@gmail.com>
2024-11-20 23:51:02 -0300

    Infinity scroll

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native';
+import { ThemedText } from "@/presentation/shared/ThemedText";
+import { ThemedView } from "@/presentation/shared/ThemedView";
+import { useState } from "react";
+import { FlatList, Image } from "react-native";
 
 const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ThemedView>
+      <FlatList
+        data={numbers}
+        renderItem={({ item }) => <ListItem number={item} />}
+        onEndReached={loadMore}
+        // Porcentaje del scroll para realizar la petición
+        onEndReachedThreshold={0.6}
+      />
+    </ThemedView>
   );
 };
+
 export default InfiniteScrollScreen;
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+  return (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+      style={{ width: "100%", height: 400 }}
+    />
+  );
+};
